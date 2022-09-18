0ba12d6b4e649330b708342581de2ddfb005f34f
yantsant <73341163+yantsant@users.noreply.github.com>
2022-09-18 17:16:31 +0400

    Second version
    - this version acceleration is 3.0-6.3%
    - the first version acceleration is 2.5-5.5%

--- code/optimized.sol
+++ code/optimized.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 contract MinContract{
     function min(uint256 a, uint256 b) public pure returns (uint256 result){
         assembly{
-            mstore(result, add(mul(eq(a, b),a), add(mul(lt(a, b),a), mul(gt(a, b),b))))
+            mstore(result, add(mul(a, add(eq(a, b), lt(a, b))), mul(gt(a, b),b)))
             return(result, 32)
         } 
     }
