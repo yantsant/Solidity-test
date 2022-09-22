c0fa1804c0bdcf095425f2e233390ca6ad5b026b
yantsant <73341163+yantsant@users.noreply.github.com>
2022-09-22 15:33:09 +0400

    Update optimized.sol
    acceleration up to 3.5-7.5%

--- code/optimized.sol
+++ code/optimized.sol
@@ -2,10 +2,11 @@
 pragma solidity ^0.8.0;
 
 contract MinContract{
-    function min(uint256 a, uint256 b) public pure returns (uint256 result){
+    function min(uint256 a, uint256 b) public pure returns (uint256){
         assembly{
-            mstore(result, add(mul(a, add(eq(a, b), lt(a, b))), mul(gt(a, b),b)))
-            return(result, 32)
-        } 
+            mstore(0x80, a)
+            mstore(0xa0, b)
+            return(add(0x80,mul(0x20,gt(a, b))), 0x20)
+        }
     }
 }
